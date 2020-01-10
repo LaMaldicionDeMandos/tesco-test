d3cbc3d561debb4acce293ab7383d269a0329351
marcelo <pasutmarcelo@gmail.com>
2020-01-09 23:52:59 -0300

    implemento el response del sync

--- src/main/java/coop/tecso/examen/controller/GoogleHomeController.java
+++ src/main/java/coop/tecso/examen/controller/GoogleHomeController.java
@@ -1,5 +1,10 @@
 package coop.tecso.examen.controller;
 
+import com.google.actions.api.smarthome.*;
+import com.google.api.client.json.Json;
+import com.google.gson.JsonObject;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
@@ -10,16 +15,47 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
+import java.util.concurrent.ExecutionException;
 
 @RestController
-public class GoogleHomeController {
+public class GoogleHomeController extends SmartHomeApp {
     private final static Logger LOG = LoggerFactory.getLogger(GoogleHomeController.class);
 	
     @PostMapping("/smarthome")
-    public ResponseEntity<?> sync(@RequestBody Map<String, Object> body, @RequestHeader Map<String, String> headers) {
+    public ResponseEntity<String> sync(@RequestBody String body, @RequestHeader Map<String, String> headers) throws ExecutionException, InterruptedException {
         LOG.info("Entrando al sync --> Body: {} --> headers: {}", body, headers);
-        String response = "ok";
-    	return new ResponseEntity<String>(response, HttpStatus.OK);
+        String response = this.handleRequest(body.toString(), headers).get();
+        LOG.info("Response: {}", response);
+    	return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
+    @Override
+    public void onDisconnect(@NotNull DisconnectRequest disconnectRequest, @Nullable Map<?, ?> map) {
+
+    }
+
+    @NotNull
+    @Override
+    public ExecuteResponse onExecute(@NotNull ExecuteRequest executeRequest, @Nullable Map<?, ?> map) {
+        return null;
+    }
+
+    @NotNull
+    @Override
+    public QueryResponse onQuery(@NotNull QueryRequest queryRequest, @Nullable Map<?, ?> map) {
+        return null;
+    }
+
+    @NotNull
+    @Override
+    public SyncResponse onSync(@NotNull SyncRequest syncRequest, @Nullable Map<?, ?> map) {
+        SyncResponse response = new SyncResponse();
+        response.setRequestId(syncRequest.requestId);
+        response.setPayload(new SyncResponse.Payload());
+        response.payload.agentUserId = "83367.15267389";
+
+        response.payload.devices = new SyncResponse.Payload.Device[0];
+
+        return response;
+    }
 }
