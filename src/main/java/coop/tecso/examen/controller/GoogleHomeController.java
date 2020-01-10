2e68a262d69854a0481aaef3c06c996603f1508c
marcelo <pasutmarcelo@gmail.com>
2020-01-10 00:42:50 -0300

    agrego un estado del device

--- src/main/java/coop/tecso/examen/controller/GoogleHomeController.java
+++ src/main/java/coop/tecso/examen/controller/GoogleHomeController.java
@@ -3,6 +3,7 @@ package coop.tecso.examen.controller;
 import com.google.actions.api.smarthome.*;
 import com.google.api.client.json.Json;
 import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
 import com.google.gson.JsonObject;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -22,11 +23,12 @@ import java.util.concurrent.ExecutionException;
 @RestController
 public class GoogleHomeController extends SmartHomeApp {
     private final static Logger LOG = LoggerFactory.getLogger(GoogleHomeController.class);
-	
+    private static boolean lightState = false;
+    //headers: {host=tecso-test.herokuapp.com, connection=close, content-type=application/json;charset=UTF-8, google-assistant-api-version=v1, authorization=Bearer 123access, user-agent=Mozilla/5.0 (compatible; Google-Cloud-Functions/2.1; +http://www.google.com/bot.html), accept-encoding=gzip,deflate,br, x-request-id=24d7f548-37b4-4bc6-ac9c-1d437c128baa, content-length=117, x-forwarded-proto=https, x-forwarded-port=443, via=1.1 vegur, connect-time=0, x-request-start=1578626459848, total-route-time=2}
     @PostMapping("/smarthome")
-    public ResponseEntity<String> sync(@RequestBody String body, @RequestHeader Map<String, String> headers) throws ExecutionException, InterruptedException {
-        LOG.info("Entrando al sync --> Body: {} --> headers: {}", body, headers);
-        String response = this.handleRequest(body.toString(), headers).get();
+    public ResponseEntity<String> onPost(@RequestBody String body, @RequestHeader Map<String, String> headers) throws ExecutionException, InterruptedException {
+        LOG.info("Entrando al post --> Body: {} --> headers: {}", body, headers);
+        String response = this.handleRequest(body, headers).get();
         LOG.info("Response: {}", response);
     	return new ResponseEntity<>(response, HttpStatus.OK);
     }
@@ -42,12 +44,23 @@ public class GoogleHomeController extends SmartHomeApp {
         return null;
     }
 
+    //Body: {"inputs":[{"intent":"action.devices.QUERY","payload":{"devices":[{"id":"L1"}]}}],"requestId":"18012506326314158884"}
     @NotNull
     @Override
     public QueryResponse onQuery(@NotNull QueryRequest queryRequest, @Nullable Map<?, ?> map) {
-        return null;
+        QueryResponse res = new QueryResponse();
+        res.setRequestId(queryRequest.requestId);
+        res.setPayload(new QueryResponse.Payload());
+
+        Map<String, Map<String, Boolean>> state = Maps.newHashMap();
+        Map<String, Boolean> m = Maps.newHashMap();
+        m.put("on", lightState);
+        state.put("L1", m);
+        res.payload.setDevices(state);
+        return res;
     }
 
+    //Body: {"inputs":[{"intent":"action.devices.SYNC"}],"requestId":"6593579405166916194"}
     @NotNull
     @Override
     public SyncResponse onSync(@NotNull SyncRequest syncRequest, @Nullable Map<?, ?> map) {
