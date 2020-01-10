91ff1907ea73ecd6d107970ac7cad045555acddc
marcelo <pasutmarcelo@gmail.com>
2020-01-10 00:19:19 -0300

    agrego un device

--- src/main/java/coop/tecso/examen/controller/GoogleHomeController.java
+++ src/main/java/coop/tecso/examen/controller/GoogleHomeController.java
@@ -2,6 +2,7 @@ package coop.tecso.examen.controller;
 
 import com.google.actions.api.smarthome.*;
 import com.google.api.client.json.Json;
+import com.google.common.collect.Lists;
 import com.google.gson.JsonObject;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
 import java.util.Map;
 import java.util.concurrent.ExecutionException;
 
@@ -54,7 +56,16 @@ public class GoogleHomeController extends SmartHomeApp {
         response.setPayload(new SyncResponse.Payload());
         response.payload.agentUserId = "83367.15267389";
 
-        response.payload.devices = new SyncResponse.Payload.Device[0];
+        response.payload.devices = new SyncResponse.Payload.Device[1];
+        SyncResponse.Payload.Device.Builder builder = new SyncResponse.Payload.Device.Builder();
+        SyncResponse.Payload.Device device = builder.setId("L1")
+                .setType("action.devices.types.LIGHT")
+                .addTrait("action.devices.traits.OnOff")
+                .setName(Arrays.asList("Luz Virtual"), "Luz V 1", Lists.newArrayList("la luz"))
+                .setWillReportState(false)
+                .build();
+        response.payload.devices[0] = device;
+
 
         return response;
     }
